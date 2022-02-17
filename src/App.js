379af891eb3e75071edf4bce722ffbe4bfda3c2c
Mikhail <mikhail.dedeiko@gmail.com>
2022-02-17 17:48:36 +0300

    added Feature

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React _ I was updated
         </a>
       </header>
     </div>
