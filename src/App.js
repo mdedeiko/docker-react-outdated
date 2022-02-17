703a8b04ca36a9c83e9546c295f373f6cb2cd91d
Mikhail <mikhail.dedeiko@gmail.com>
2022-02-17 17:50:30 +0300

    added Feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>I was updated</p>
+        <p>I was updated_twice</p>
         <a
           className="App-link"
           href="https://reactjs.org"
